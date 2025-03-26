85a0f755515c5d83a35a3d91a183612fd40ac8f1
ananya-mat <gamchngr21@outlook.com>
2025-03-26 14:38:08 +0530

    Add files via upload

--- gauss_elim.m
+++ gauss_elim.m
@@ -1,16 +1,30 @@
-A=[1 1 -1; 2 -1 1; 3 1 -1];
+A=[1 1 -1; 2 1 1; 3 1 -1];
 B=[1;-2; 3];
-a=[A B];
-for k=1:2
-    for i=k+1:3
-        ftr=a(i,k)/a(k,k);
-        a(i,:)=a(i,:)-ftr*a(k,:);
+Ab = [A, B];
+[n, m] = size(Ab);
+
+% Forward elimination
+for k = 1:n-1
+    for i = k+1:n
+        factor = Ab(i, k) / Ab(k, k);
+        for j = k:m
+            Ab(i, j) = Ab(i, j) - factor * Ab(k, j);
+        end
     end
 end
 
-n=3;
-x=zeros(n,1);
-for i=n:-1:1
-    x(i) = a(i,end)-sum(a(i,i+1:n)*x(i+1:n))/a(i,i);
+% Back substitution
+x = zeros(n, 1);
+x(n) = Ab(n, m) / Ab(n, n);
+
+for i = n-1:-1:1
+    sum = 0;
+    for j = i+1:n
+        sum = sum + Ab(i, j) * x(j);
+    end
+    x(i) = (Ab(i, m) - sum) / Ab(i, i);
 end
-disp(x)
+
+% Display the solution
+disp('Solution x:');
+disp(x);
